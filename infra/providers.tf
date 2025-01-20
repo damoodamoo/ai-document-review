ee3cf2aa86e95722bdd238819813a64235a51f05
Lyon Till <158992+ljtill@users.noreply.github.com>
2025-01-20 16:59:17 +0000

    Pin major versions (#27)

--- infra/providers.tf
+++ infra/providers.tf
@@ -2,23 +2,21 @@ terraform {
   required_version = "~> 1.0"
 
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.0"
-    }
     azapi = {
       source  = "azure/azapi"
       version = "~> 2.0"
     }
-
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "3.0.2"
+      version = "~> 3.0"
     }
-
     random = {
       source  = "hashicorp/random"
-      version = "3.4.3"
+      version = "~> 3.0"
     }
   }
 }
